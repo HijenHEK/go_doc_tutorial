ec365790e92a46c81a85951bca32132381e788f9
Hijen <hijenhek@gmail.com>
2022-04-20 10:46:09 +0100

    create a module : handle errors

--- create_a_module/hello/hello.go
+++ create_a_module/hello/hello.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/hijenhek/go_doc_tutorial/create_a_module/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Hijen")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(message)
 }
