1a4e499e0e07b89bd42f499c4f5d90e4c42d0fc6
hijen <hijenhek@gmail.com>
2022-04-25 22:32:14 +0100

    generics : declaring a type constraint

--- generics/main.go
+++ generics/main.go
@@ -20,13 +20,17 @@ func SumFloats(m map[string]float64) float64 {
 	return s
 }
 
-func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+func SumNumbers[K comparable, V Number](m map[K]V) V {
 	var s V
 	for _, v := range m {
 		s += v
 	}
 
 	return s
+}
+
+type Number interface {
+	int64 | float64
 }
 
 func main() {
@@ -45,7 +49,7 @@ func main() {
 		SumFloats(floats))
 
 	fmt.Printf("Generic Sums: %v and %v\n",
-		SumIntsOrFloats(ints),
-		SumIntsOrFloats(floats))
+		SumNumbers(ints),
+		SumNumbers(floats))
 
 }
